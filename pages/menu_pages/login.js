b0f20f0dc0208278dede46a6ef3ac471b479f634
ivanisimys <ivandomrist@gmail.com>
2026-01-29 20:53:16 +0300

    login upd2

--- pages/menu_pages/login.js
+++ pages/menu_pages/login.js
@@ -8,7 +8,23 @@ form.addEventListener("submit", function(event) {
     // Считываем данные с каждого поля по его имени, используя метод get()
     let username = data.get("us");
     let userPassword = data.get("password");
-    console.log(username);
-    console.log(userPassword)
+    fetch('http://web4.informatics.ru:82/api/81eabc863ed6d21e46f5b1988c463467/login')
+        .then(function(response) {
+             if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(function(data){
+
+            let check_pass = data[username]
+            if (userPassword == check_pass){
+                log.style.display = 'none'
+                console.log('Вход выплнен!')
+            }
+            else{
+                console.log('Неверные данные!')
+            }
+        })
+    
 })
 
